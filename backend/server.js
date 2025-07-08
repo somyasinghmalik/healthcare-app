922711f84067be764e2d36b4a5eca154fdeeb6fc
somya malik <somyasinghmalik@gmail.com>
2025-07-08 05:58:14 +0530

    dd

--- backend/server.js
+++ backend/server.js
@@ -35,13 +35,7 @@ app.get('/', (req, res) => {
 
 const path = require('path');
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-// For any route not handled by API, serve React
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-});
 
 // ✅ Connect to MongoDB and start the server
 mongoose.connect(process.env.MONGO_URI, {
